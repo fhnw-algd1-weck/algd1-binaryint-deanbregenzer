ffd5b9c137667394cef97b74dee9b7f780b048bd
Dean Bregenzer <dean.bregenzer@students.fhnw.ch>
2025-02-25 09:09:35 +0100

    - to string hat noch keine Implementierung für negative Zahlen - from String funktioniert für alle texte mit der Länge 8

--- src/main/java/ch/fhnw/algd1/converters/binary/BinConverter.java
+++ src/main/java/ch/fhnw/algd1/converters/binary/BinConverter.java
@@ -2,14 +2,33 @@ package ch.fhnw.algd1.converters.binary;
 
 public class BinConverter {
 	public static String toString(int x) {
-		// TODO expect x to be in range [-128, 127], return string with 8 binary
-		// digits representing x in 2-complement
-		return "00000000";
+		String s = "";
+
+		while (x > 0){
+			if (x % 2 == 0) {
+				s = "0" + s;
+			} else {
+				s = "1" + s;
+			}
+		}
+		return s;
 	}
 
 	public static int fromString(String text) {
-		// TODO expect text to contain 8 binary digits, parse to int value in
-		// 2-complement
-		return 0;
+		int result = 0;
+		if (text.length() == 8){
+			if (text.charAt(0) == '1') {
+				result -= ((int) Math.pow(2, text.length() - 1));
+			}
+			for (int i = 1; i < text.length(); i++) {
+				if (text.charAt(i) == '1') {
+					result += (int) Math.pow(2, text.length() - (i + 1));
+				}
+			}
+		} else {
+			System.out.println("nicht gültige Zahl");
+		}
+		return result;
 	}
 }
+
