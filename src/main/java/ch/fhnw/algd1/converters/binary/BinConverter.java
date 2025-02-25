288b536f334f1c8bcc842c48a4993f37901981f0
Dean Bregenzer <dean.bregenzer@students.fhnw.ch>
2025-02-25 13:03:20 +0100

    loop entfernt da x nicht aktualisiert wurde

--- src/main/java/ch/fhnw/algd1/converters/binary/BinConverter.java
+++ src/main/java/ch/fhnw/algd1/converters/binary/BinConverter.java
@@ -3,16 +3,28 @@ package ch.fhnw.algd1.converters.binary;
 public class BinConverter {
 	public static String toString(int x) {
 		String s = "";
+		boolean negativ = false;
 
-		while (x > 0){
+		if (x < 0) {
+			negativ = true;
+			x = -x;
+		}
+		while (x > 0) {
 			if (x % 2 == 0) {
 				s = "0" + s;
+				x /= 2;
 			} else {
 				s = "1" + s;
+				x /= 2;
 			}
 		}
-		return s;
+
+
+
+
+		return String.format("%8s", s).replace(" ", "0");
 	}
+
 
 	public static int fromString(String text) {
 		int result = 0;
