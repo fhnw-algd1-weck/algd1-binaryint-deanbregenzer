75074564d2c1ef3606cc0b9b44101b28fc98931c
Dean Bregenzer <dean.bregenzer@students.fhnw.ch>
2025-02-25 13:55:36 +0100

    Negierte Zahlen werden nun berechnet und umgewandelt. Es wird ein Array erstellt welches die Binaries invertiert. Danach wird 1 zum Ergebnis addiert mit einem einfachen loop

--- src/main/java/ch/fhnw/algd1/converters/binary/BinConverter.java
+++ src/main/java/ch/fhnw/algd1/converters/binary/BinConverter.java
@@ -4,11 +4,11 @@ public class BinConverter {
 	public static String toString(int x) {
 		String s = "";
 		boolean negativ = false;
-
 		if (x < 0) {
 			negativ = true;
 			x = -x;
 		}
+
 		while (x > 0) {
 			if (x % 2 == 0) {
 				s = "0" + s;
@@ -19,11 +19,36 @@ public class BinConverter {
 			}
 		}
 
+		s = String.format("%8s", s).replace(" ", "0");
+		String ss = "";
 
+		// Wenn die Zahl negativ ist, berechnen wir das Zweierkomplement
+		if (negativ) {
+			// 1. Invertiere alle Bits (Einerkomplement)
+			char[] chars = s.toCharArray();
+			for (int i = 0; i < chars.length; i++) {
+				chars[i] = (chars[i] == '0') ? '1' : '0';
+			}
 
+			// 2. Addiere 1 zum Ergebnis (Zweierkomplement)
+			for (int i = chars.length - 1; i >= 0; i--) {
+				if (chars[i] == '0') {
+					chars[i] = '1';
+					break;
+				} else {
+					chars[i] = '0';
+				}
+			}
 
-		return String.format("%8s", s).replace(" ", "0");
+			// Rückgabe des Zweierkomplements als String
+			s = new String(chars);
+		}
+
+		return s;
 	}
+
+
+
 
 
 	public static int fromString(String text) {
